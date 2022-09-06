1649db072a6e7d212b2a92641c9b65e5a080bfbb
GustavoSolar <f170015@gmail.com>
2022-09-06 00:06:22 -0500

    agregando el contenido del dia tres

---
+++ python/curso_python_16_days/03_dia/listas/listas.py
@@ -0,0 +1,7 @@
+"""
+- Las listas pueden estar compuestas por distintos tipos de datos
+- Se escriben entre corchetes
+- Estan separados entre si por comas ( , )
+- Se pueden anidar listas
+- Son mutables
+"""
